a3fecfd895f96e3d803407cbed70e23b02ac7ab5
Calvin Seidle <seidlecalvin@gmail.com>
2017-03-29 23:12:17 +0100

    Initial Commit

---
+++ blog/db/migrate/20170329215324_add_details_to_user.rb
@@ -0,0 +1,9 @@
+class AddDetailsToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :username, :string
+    add_column :users, :bio, :text
+    add_column :users, :dob, :date
+  end
+end
